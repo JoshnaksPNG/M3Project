398add9a500aee7ff518030347dbf3e0589893af
JsephGriff <JsephGriff@outlook.com>
2022-02-26 18:52:12 -0800

    Added a 1 to simplify.

--- Model2.js
+++ Model2.js
@@ -4,5 +4,5 @@ let P = 0;
 
 function DeenMethod(I, B, T, x, X, y, Y, z, Z, C, H, D) {
  U = T(X * x + Y * y + Z * z);
- P = I * B * (-(1 / (Y + ((Y * C) / 60 * H)) + 1)) * U * D;
+ P = I * B * (-1(1 / (Y + ((Y * C) / 60 * H)) + 1)) * U * D;
 }
