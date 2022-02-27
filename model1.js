aa6e33aba3467bd8f4d2a9f3c1dbe7102e31a62a
JsephGriff <JsephGriff@outlook.com>
2022-02-26 21:15:41 -0800

    Final Code Cleanup

--- model1.js
+++ model1.js
@@ -1,16 +1,3 @@
-let pop = {
-    "Mining, Logging and Construction": 109600,
-    "Manufacturing": 142200,
-    "Trade, Transportation and Utilities": 332600,
-    "Information": 13900,
-    "Financial Activities": 87600,
-    "Professional and Buisness Services": 277500,
-    "Education and Health Services": 223500,
-    "Leisure and Hospitality": 13300,
-    "Other Services": 59300,
-    "Government": 206700
-}
-/*
 let popWeights = {
     "Mining, Logging and Construction": { "Farming, Fishing, Forestry": 0.35, "Building and Grounds Cleaning": 0.2, "Construction and Extraction": 0.45 },
     "Manufacturing": { "Production": 0.7, "Architechture and Engineering": 0.3, },
@@ -23,7 +10,44 @@ let popWeights = {
     "Other Services": { "Life Physical and Social Science": 1 },
     "Government": { "Community and Social Service": 0.2, "Protective Services": 0.5, "Transportation and Material Moving": 0.3 }
 }
-*/
+
+let pop = {
+    "Mining, Logging and Construction": 109600,
+    "Manufacturing": 142200,
+    "Trade, Transportation and Utilities": 332600,
+    "Information": 13900,
+    "Financial Activities": 87600,
+    "Professional and Buisness Services": 277500,
+    "Education and Health Services": 223500,
+    "Leisure and Hospitality": 13300,
+    "Other Services": 59300,
+    "Government": 206700
+}
+let data = {
+    "Computer and Math": 1,
+    "Education and Training": 0.98,
+    "Legal": 0.97,
+    "Buisness and Finance": 0.88,
+    "Management": 0.87,
+    "Art and Design": 0.76,
+    "Office and Admin": 0.65,
+    "Architechture and Engineering": 0.61,
+    "Life Physical and Social Science": 0.54,
+    "Community and Social Service": 0.37,
+    "Sales and Related": 0.28,
+    "Personal Care and Service": 0.26,
+    "Protective Services": 0.6,
+    "Healthcare Practitioners": 0.5,
+    "Transportation and Material Moving": 0.3,
+    "Healthcare Support": 0.2,
+    "Farming, Fishing, Forestry": 0.1,
+    "Production": 0.1,
+    "Installation and Maitenence": 0.1,
+    "Construction and Extraction": 0.0,
+    "Food Preperation": 0.0,
+    "Building and Grounds Cleaning": 0.0
+}
+
 //for each sub catagory, label the percent
 
 
@@ -41,11 +65,7 @@ getEstimatedPopulation("Barry, Wales");
 
 
 function getEstimatedPopulation(name) {
-    //TAKE EACH CATAGOY
-    //MULTIPY WEIGHT * POPULATION * REMOTEREADY 
-    //SUM THEM ALL FOR EACH CATAGORy
-    //DIVIDE BY TOTAL CATAGOY COUNT
-    //calculate percent that applies to each population catagory
+    
     console.log("Estimated % of individuals who can work remotly for the city of " + name);
 
     let cTotal = 0;
@@ -79,30 +99,7 @@ function getEstimatedPopulation(name) {
 }
 
 function UpdatePopulationList(pMLC, pMAN, pTTU, pINF, pFIN, pPBS, pEHS, pLAH, pOTH, pGOV) {
-    let data = {
-        "Computer and Math": pMLC/pMLC,
-        "Education and Training": 0.98,
-        "Legal": 0.97,
-        "Buisness and Finance": 0.88,
-        "Management": 0.87,
-        "Art and Design": 0.76,
-        "Office and Admin": 0.65,
-        "Architechture and Engineering": 0.61,
-        "Life Physical and Social Science": 0.54,
-        "Community and Social Service": 0.37,
-        "Sales and Related": 0.28,
-        "Personal Care and Service": 0.26,
-        "Protective Services": 0.6,
-        "Healthcare Practitioners": 0.5,
-        "Transportation and Material Moving": 0.3,
-        "Healthcare Support": 0.2,
-        "Farming, Fishing, Forestry": 0.1,
-        "Production": 0.1,
-        "Installation and Maitenence": 0.1,
-        "Construction and Extraction": 0.0,
-        "Food Preperation": 0.0,
-        "Building and Grounds Cleaning": 0.0
-    }
+  
     pop["Mining, Logging and Construction"] = pMLC;
     pop["Manufacturing"] = pMAN;
     pop["Trade, Transportation and Utilities"] = pTTU;
